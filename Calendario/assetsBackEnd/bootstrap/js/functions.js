c8e9c1b9e6954bbf66e7e696d46c0d6dde2d80a7
William Ballesteros Blanco <ballesteroswilliam@avaya.com>
2017-09-26 22:33:44 -0500

    Calendario avaya dos

---
+++ Calendario/assetsBackEnd/bootstrap/js/functions.js
@@ -0,0 +1,336 @@
+﻿//tipo de mensajes ->  info, success, warning, error
+function message(texto, titulo, tipo) {
+
+    BootstrapDialog.show({
+        title: titulo,
+        message: texto,
+        cssClass: 'type-' + tipo
+    });
+    return false;
+
+}
+
+var modalLoading;
+modalLoading = modalLoading || (function () {
+    return {
+        show: function (mensaje) {
+            $("#loading").fadeIn("slow");
+        },
+        hide: function () {
+            $("#loading").fadeOut("slow");
+        },
+    };
+})();
+
+var searchIntoJson = function (obj, column, value) {
+    var results = [];
+    var searchField = column;
+    var searchVal = value;
+    for (var i = 0 ; i < obj.length ; i++) {
+        if (obj[i][searchField] == searchVal) {
+            results.push(obj[i]);
+
+        }
+    }
+    return results;
+}
+
+var searchFirstIntoJson = function (obj, column, value) {
+    var searchField = column;
+    var foo = [];
+    var searchVal = value;
+    for (var i = 0 ; i < obj.length ; i++) {
+        if (obj[i][searchField] == searchVal) {
+            foo = obj[i];
+        }
+    }
+    return foo;
+}
+
+var CambiarConstrasena = function (actual, nueva, conf) {
+    if (actual !== "" && nueva !== "" && conf !== "") {
+        if (nueva === conf) {
+            var datae = { 'actual': actual, 'nueva': nueva };
+            $.ajax({
+                type: "POST",
+                url: "register.asmx/CambiarContrasena",
+                contentType: "application/json; charset=utf-8",
+                data: JSON.stringify(datae),
+                dataType: "json",
+                success: function (resultado) {
+                    var tales = resultado.d.indexOf("NoSession");
+                    if (tales !== -1) {
+                        document.location.href = "login.html?BackPageSession=NoSession";
+                    }
+                    tales = resultado.d.indexOf("ok");
+                    if (tales != -1) {
+                        titulo = (lang === "ESP") ? "Atención" : "Attention";
+                        mensaje = (lang === "ESP") ? "Contraseña cambiada correctamente" : "Password changed successfully";
+                        message(mensaje, titulo, "success");
+                        $('#recoveryModal').modal('hide');
+                    } else {
+                        titulo = (lang === "ESP") ? "Atención" : "Attention";
+                        mensaje = (lang === "ESP") ? "Se ha presentado un error, por favor trate más tarde" : "An error occurred, please try later";
+                        message(mensaje, titulo, "success");
+
+                    }
+                },
+                error: function (error) {
+                    titulo = (lang === "ESP") ? "Atención" : "Attention";
+                    mensaje = (lang === "ESP") ? "Se ha presentado un error, por favor trate más tarde" : "An error occurred, please try later";
+                    message(mensaje, titulo, "success");
+
+                }
+            });
+        } else {
+            titulo = (lang === "ESP") ? "Atención" : "Attention";
+            mensaje = (lang === "ESP") ? "La actual y la nueva contraseña no coinciden" : "The passwords do not match";
+            message(mensaje, titulo, "danger");
+        }
+    } else {
+
+        titulo = (lang === "ESP") ? "Atención" : "Attention";
+        mensaje = (lang === "ESP") ? "Todos los datos son requeridos" : "Please fill in all requested data";
+        message(mensaje, titulo, "warning");
+    }
+
+
+    return false;
+}
+var nombreUsuario = "";
+var validarSession = function () {
+    var datae = { 'data': 'data' };
+    $.ajax({
+        type: "POST",
+        url: "register.asmx/ValidarSession",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(datae),
+        dataType: "json",
+        success: function (result) {
+            var tales = result.d.indexOf("NoSession");
+            if (tales !== -1) {
+                document.location.href = "login.html?BackPageSession=NoSession";
+            }
+            nombreUsuario = result.d;
+            $("#usuarioNick").html(result.d);
+        },
+        error: function (error) {
+            document.location.href = "login.html?BackPageSession=NoSession";
+        }
+    });
+    return false;
+}
+
+
+
+var searchCharIntoJson = function (obj, column, value) {
+    var results = [];
+    var valueField;
+    var searchField = column;
+    var searchVal = value;
+    searchVal = searchVal.toLowerCase();
+    for (var i = 0 ; i < obj.length ; i++) {
+        valueField = obj[i][searchField];
+        valueField = valueField.toLowerCase();
+        if (valueField.indexOf(searchVal) !== -1) {
+            results.push(obj[i]);
+
+        }
+    }
+    return results;
+}
+
+
+
+//Obtener los parametros de la URL
+var QueryString = function () {
+    var query_string = {};
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (typeof query_string[pair[0]] === "undefined") {
+            query_string[pair[0]] = pair[1];
+        } else if (typeof query_string[pair[0]] === "string") {
+            var arr = [query_string[pair[0]], pair[1]];
+            query_string[pair[0]] = arr;
+        } else {
+            query_string[pair[0]].push(pair[1]);
+        }
+    }
+    return query_string;
+}();
+
+function textCounter(field, cnt, maxlimit) {
+    var cntfield = document.getElementById(cnt);
+    if (field.value.length > maxlimit) { // if too long...trim it!
+        //field.value = field.value.substring(0, maxlimit);
+        cntfield.value = maxlimit - field.value.length;
+        $('#' + cnt).css('color', '#cc0000');
+    }
+        // otherwise, update 'characters left' counter
+    else {
+        cntfield.value = maxlimit - field.value.length;
+        $('#' + cnt).css('color', '#000000');
+    }
+}
+
+
+function eliminarUltimoCaracter(texto, caracter) {
+    if ((texto.substring(texto.length - 1, texto.length)) === caracter) {
+        texto = texto.substring(0, texto.length - 1);
+    }
+    return texto;
+}
+
+function eliminarPrimerCaracter(texto, caracter) {
+    if ((texto.substring(0, 1)) === caracter) {
+        texto = texto.substring(1, texto.length);
+    }
+    return texto;
+}
+
+if (!Array.prototype.indexOf) {
+    Array.prototype.indexOf = function (obj, start) {
+        for (var i = (start || 0), j = this.length; i < j; i++) {
+            if (this[i] === obj) { return i; }
+        }
+        return -1;
+    };
+}
+
+function getInternetExplorerVersion() {
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf('MSIE ');
+    var trident = ua.indexOf('Trident/');
+    if (msie > 0) {
+        // Si es IE 10 o más viejo => retorna el número de versión
+        return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
+
+    if (trident > 0) {
+        // Si es IE 11 o más nuevo => retorna el número de versión
+        var rv = ua.indexOf('rv:');
+        return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+
+    // otro browser
+    return false;
+}
+
+//Validaciones de campos
+function validate(valor) {
+    if (valor == null || valor.length < 1 || /^\s+$/.test(valor)) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+function replaceAll(string, find, replace) {
+    return string.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+}
+
+//Elimina los caracteres especiales en un texto para poderlos mostrar sin errores
+function escapeRegExp(string) {
+    return string.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1");
+}
+
+//Validar campo email
+function validateEmail(valor) {
+    var correo = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (!correo.exec(valor)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function validateNumber(valor) {
+    if (validate(valor)) {
+        if (isNaN(valor)) {
+            return false;
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
+}
+
+function setSelectByValue(eID, val) {
+    var ele = document.getElementById(eID);
+    for (var ii = 0; ii < ele.length; ii++)
+        if (ele.options[ii].value === val) {
+            ele.options[ii].selected = true;
+            return true;
+        }
+    return false;
+}
+
+function setSelectByValueDiffType(eID, val) {
+    val = String(val);
+    var ele = document.getElementById(eID);
+    for (var ii = 0; ii < ele.length; ii++)
+        if (ele.options[ii].value === val) {
+            ele.options[ii].selected = true;
+            return true;
+        }
+    return false;
+}
+
+function setSelectByText(eID, text) {
+    var ele = document.getElementById(eID);
+    for (var ii = 0; ii < ele.length; ii++)
+        if (ele.options[ii].text == text) {
+            ele.options[ii].selected = true;
+            return true;
+        }
+    return false;
+}
+
+var validarExtension = function (nombre) {
+    var ext = (nombre.substring(nombre.lastIndexOf(".") + 1)).toUpperCase();
+    var respuesta = 0;
+    var extension = new Object();
+    extension.EXE = "EXE";
+    extension.COM = "COM";
+    extension.VB = "VB";
+    extension.VBS = "VBS";
+    extension.VBEE = "VBE";
+    extension.CMDD = "CMD";
+    extension.BATT = "BAT";
+    extension.WSS = "WS";
+    extension.WSFF = "WSF";
+    extension.SCRR = "SCR";
+    extension.SHS = "SHS";
+    extension.PIF = "PIF";
+    extension.HTA = "HTA";
+    extension.JAR = "JAR";
+    extension.JS = "JS";
+    extension.JSE = "JSE";
+    extension.LNK = "LNK";
+
+    for (var i in extension) {
+        if (extension[i] === ext) {
+            respuesta = parseInt(respuesta) + 1;
+            break;
+        }
+        else {
+            respuesta = parseInt(respuesta) + 0;
+        }
+    }
+    if (respuesta === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
+
+
+
+
---
+++ Calendario/assetsBackEnd/js/funcionesComunes.js
@@ -0,0 +1,305 @@
+﻿
+var correoUsuario;
+var loading;
+var done = "";
+
+
+loading = loading || (function () {
+    var pleaseWaitDiv = $('<div class="modal" id="pleaseWaitDialog" data-backdrop="static" data-keyboard="false"><div class="modal-dialog" style="width:250px!important;top: 90px;"> <div class="modal-content" style="background-color:rgba(255, 255, 255,1);border-radius: 5px;"> <div class="modal-header" style="padding-top:5px!important;padding-bottom:5px!important;padding-left:15px;padding-right:15px;border-bottom: 0px!important;"> <h4 style="text-align:center;"><strong style="color:#cc0000;">Loading...</strong> &nbsp;&nbsp;&nbsp; <img src="images/gears32.gif" /></h4></div></div></div></div>');
+    return {
+        show: function (texto) {
+            if (texto !== undefined) {
+                pleaseWaitDiv = $('<div class="modal" id="pleaseWaitDialog" data-backdrop="static" data-keyboard="false"><div class="modal-dialog" style="width:500px!important;top: 90px;"> <div class="modal-content" style="background-color:rgba(255, 255, 255,1);border-radius: 5px;"> <div class="modal-header" style="padding-top:5px!important;padding-bottom:5px!important;padding-left:15px;padding-right:15px;border-bottom: 0px!important;"> <h4 style="text-align:center;"><strong style="color:#cc0000;">' + texto + '</strong> &nbsp;&nbsp;&nbsp; <img src="images/gears32.gif" /></h4></div></div></div></div>');
+            } else {
+                pleaseWaitDiv = $('<div class="modal" id="pleaseWaitDialog" data-backdrop="static" data-keyboard="false"><div class="modal-dialog" style="width:250px!important;top: 90px;"> <div class="modal-content" style="background-color:rgba(255, 255, 255,1);border-radius: 5px;"> <div class="modal-header" style="padding-top:5px!important;padding-bottom:5px!important;padding-left:15px;padding-right:15px;border-bottom: 0px!important;"> <h4 style="text-align:center;"><strong style="color:#cc0000;">Loading...</strong> &nbsp;&nbsp;&nbsp; <img src="images/gears32.gif" /></h4></div></div></div></div>');
+            }
+            pleaseWaitDiv.modal("show");
+        },
+        hide: function () {
+            pleaseWaitDiv.modal("hide");
+        }
+    };
+})();
+
+var GetUserName = function () {
+    var datae = { 'vista': "fake" };
+    $.ajax({
+        type: "POST",
+        url: "WebServices/Utilities.asmx/GetUserName",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify(datae),
+        dataType: "json",
+        success: function (result) {
+            var data = result.d;
+            var dataJson = $.parseJSON(data);
+            if (dataJson.status === "ok") {
+                $("#username").html("<strong>" + dataJson.username + "</strong>");
+                $("#userName").html("<strong>" + dataJson.username + "</strong>");
+                if (dataJson.adminTranslation === "YES") {
+                    $("#translationAdmin").html("Translations");
+                }
+            } else {
+                if (dataJson.data === "NoSession") {
+                    document.location.href = "index.html?BackPageSession=NoSession";
+                } else {
+                    $("#username").val("User");
+                }
+            }
+
+
+        },
+        error: function (error) {
+            $("#username").val("User");
+        }
+    });
+    return false;
+}
+
+
+var validarPeso = function (idFile) {
+    var archivo = $('#' + idFile);
+    var picsize = (archivo[0].files[0].size);
+    if (picsize > 10485760) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+var validarExtensionImg = function (nombre) {
+    var ext = (nombre.substring(nombre.lastIndexOf(".") + 1)).toUpperCase();
+    if (ext === "JPEG" || ext === "JPG" || ext === "" || ext === "PNG") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function validateAvayaEmail(email) {
+    email = email.toUpperCase();
+    var splitted = email.match("^(.+)@AVAYA\.COM$");
+    if (splitted == null) return false;
+    if (splitted[1] != null) {
+        var regexpUser = /^\"?[\w-_\.]*\"?$/;
+        if (splitted[1].match(regexpUser) == null) return false;
+        return true;
+    }
+    return false;
+}
+
+var options = [];
+
+$('.dropdown-menu a').on('click', function (event) {
+
+    var $target = $(event.currentTarget),
+        val = $target.attr('data-value'),
+        $inp = $target.find('input'),
+        idx;
+
+    if ((idx = options.indexOf(val)) > -1) {
+        options.splice(idx, 1);
+        setTimeout(function () { $inp.prop('checked', false) }, 0);
+    } else {
+        options.push(val);
+        setTimeout(function () { $inp.prop('checked', true) }, 0);
+    }
+
+    $(event.target).blur();
+
+    console.log(options);
+
+    $("#tags").html(options);
+
+
+
+    return false;
+});
+
+
+
+
+var limpiarFormulario = function (obj) {
+    for (var i in obj) {
+        $("#" + i).val("");
+        $("#" + i + "Msg").removeClass("has-success");
+        $("#" + i + "Ch").removeClass("has-success");
+        
+    }
+}
+
+var limpiarFormularioAdd = function (obj) {
+    for (var i in obj) {
+        $("#" + i + "Add").val("");
+        $("#" + i + "AddMsg").removeClass("has-success");
+
+    }
+}
+
+var loadSelects = function (item, id) {
+    $("#" + id).empty();
+    $("#" + id).append('<option value="" selected="selected"></option>');
+    $.each(item, function (i, valor) {
+        $("#" + id).append('<option value="' + valor.Id + '">' + valor.Nombre + '</option>');
+    });
+    $("#loading").fadeOut("slow");
+}
+
+function validarFormulario(obj) {
+    var respuesta = 0;
+    for (var i in obj) {
+        if (obj[i] == null || obj[i].length < 1 || /^\s+$/.test(obj[i])) {
+                respuesta = respuesta + 1;
+                $("#" + i + "Msg").removeClass("has-success");
+                $("#" + i + "Msg").addClass("has-error");
+                $("#" + i + "Ch").removeClass("has-success");
+                $("#" + i + "Ch").addClass("has-error");
+            
+        } else {
+            if (i.indexOf("Email") !== -1) {
+                var correo = /^[A-Za-z][A-Za-z0-9_\.-]*@[A-Za-z0-9_-]+\.[A-Za-z0-9_.]+[A-za-z]$/;
+                if (!correo.exec(obj[i])) {
+                    respuesta = respuesta + 1;
+                    $("#" + i + "Msg").removeClass("has-success");
+                    $("#" + i + "Msg").addClass("has-error");
+                    $("#" + i + "Ch").removeClass("has-success");
+                    $("#" + i + "Ch").addClass("has-error");
+                } else {
+                    respuesta = respuesta + 0;
+
+                    $("#" + i + "Msg").removeClass("has-error");
+                    $("#" + i + "Msg").addClass("has-success");
+                    $("#" + i + "Ch").removeClass("has-error");
+                    $("#" + i + "Ch").addClass("has-success");
+                }
+            } else {
+                respuesta = respuesta + 0;
+                $("#" + i + "Msg").removeClass("has-error");
+                $("#" + i + "Msg").addClass("has-success");
+                $("#" + i + "Ch").removeClass("has-error");
+                $("#" + i + "Ch").addClass("has-success");
+            }
+
+        }
+    }
+    if (respuesta === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+$.fn.serializeObject = function () {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function () {
+        if (o[this.name]) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
+var limpiarFormularioV2 = function (obj) {
+    for (var i in obj) {
+        $("#" + i).val("");
+        $("#" + i + "Msg").removeClass("has-success");
+        $("#" + i + "Msg").removeClass("has-error");
+    }
+}
+
+function validarFormularioV2(obj, required) {
+    var respuesta = 0;
+    $.each(required, function (index, value) {
+        $.each(obj, function (key, value2) {
+            if (value === key) {
+                if (value2 == null || value2.length < 1 || /^\s+$/.test(value2) || value2 == "? undefined:undefined ?" || value2 == "?") {
+                    respuesta = respuesta + 1;
+                    $("#" + value + "Msg").removeClass("has-success");
+                    $("#" + value + "Msg").addClass("has-error");
+                } else {
+                    if (value.indexOf("Email") !== -1) {
+                        var correo = /^[A-Za-z][A-Za-z0-9_\.-]*@[A-Za-z0-9_-]+\.[A-Za-z0-9_.]+[A-za-z]$/;
+                        if (!correo.exec(value2)) {
+                            respuesta = respuesta + 1;
+                            $("#" + value + "Msg").removeClass("has-success");
+                            $("#" + value + "Msg").addClass("has-error");
+                        } else {
+                            respuesta = respuesta + 0;
+
+                            $("#" + value + "Msg").removeClass("has-error");
+                            $("#" + value + "Msg").addClass("has-success");
+                        }
+                    } else {
+                        respuesta = respuesta + 0;
+                        $("#" + value + "Msg").removeClass("has-error");
+                        $("#" + value + "Msg").addClass("has-success");
+                    }
+
+                }
+            }
+        });
+
+    });
+
+    if (respuesta === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+var formatMesDiaAnio = function(fecha) {
+    var date = new Date(fecha);
+    var fechaFormato = (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
+    return fechaFormato;
+}
+
+function downloadFile(file, carpeta) {
+    var iframe = document.getElementById("hiddenDownloader");
+    if (iframe === null) {
+        iframe = document.createElement('iframe');
+        iframe.id = "hiddenDownloader";
+        iframe.style.visibility = 'hidden';
+        document.body.appendChild(iframe);
+    }
+    iframe.src = "GenericHandlers/DownloadFileTranslation.ashx?file=" + file + "&carpeta=" + carpeta;
+}
+
+$(document).ready(function () {
+    // Optimalisation: Store the references outside the event handler:
+    var $window = $(window);
+
+    //function checkWidth() {
+    //    var windowsize = $window.width();
+    //    if (windowsize < 440) {
+    //        $(".navbar-brand").html("<span style=\"width:220px!important;\" data-toggle=\"tooltip\" data-placement=\"bottom\" title=\"Avaya Marketing Creative Services Request System\">AVAYA MCS</span>");
+    //        $("#Exit").html("<i class='fa fa-sign-out'></i>");
+    //    } else {
+    //        $(".navbar-brand").html("Avaya Marketing Creative Services Request System");
+    //        $("#Exit").html("</i><span>&nbsp;&nbsp;&nbsp;Sign Out</span>");
+    //    }
+    //}
+
+    //// Execute on load
+    //checkWidth();
+    // Bind event listener
+    //$(window).resize(checkWidth);
+
+    $( "#aside" ).load( "headerAside.html", function( response, status, xhr ) {
+        if ( status == "error" ) {
+            var msg = "Sorry but there was an error: ";
+            $( "#error" ).html( msg + xhr.status + " " + xhr.statusText );
+        }
+    });
+});
+
+try {
+    $("[name='publish']").bootstrapSwitch();
+} catch (e) {
+    var error = e;
+}
